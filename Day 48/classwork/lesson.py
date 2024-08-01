91a28781a26c1f12e5b901bb7038f02beeb55e5b
ereklekaragozishvili <erekleyaragozishvili61@gmail.com>
2024-08-01 17:19:40 +0400

    origin main

---
+++ Day 48/classwork/lesson.py
@@ -0,0 +1,57 @@
+# 1
+# შევქმნათ ცარიელი სია, მომხმარებელს შევეკითხოთ 1000 ჯერ თუ რისი დამატება სურს სიაში 
+# numbers = []
+# for i in range(1000):
+#     question = input( " what would you like to add to the list ?:  ")
+#     print(question)
+
+
+# 2
+# შევქმანათ ცარიელი სია, სადაც დავამატებთ მომხარებლის მიერ არჩეულ სიტყვას მაგრამ ყურადღებით
+#  იყავით რომ ეს სიტყვა არ აღემატებოდეს 6 ასოს
+
+
+# def My_Function(user):
+#     word = []
+#     len_user = len(user)
+
+#     if len_user <= 6:
+#         word.append(user)
+
+#     return word
+
+# print(My_Function('erekle'))
+
+
+# 3
+# შევქმნათ ცარიელი სია, სიაში დავამატოთ, კიდევ 2 სია და გადავატაროთ for loop ში
+
+# andria = []
+# nika = [1,2,3]
+# gio =[4,5,6]
+
+# andria.append(nika)
+# andria.append(gio)
+# for i in andria:
+#     print(i)
+
+# 4
+# შექმნილი არის სია სადაც წერია, ჩვენი სახელი და გვარი, str სახით
+# ჩვენი მიზანია, რომ წამოვიღოთ ეს სტრინგები და შევართოთ ერთ ცვლადში
+
+# name = ["erekle","karagozishvili"]
+# full_name = ""
+# for i in name:
+#     full_name += i
+# print(full_name)
+
+
+# 5
+# შექმენით ქართული მამაპაპური list დაარქვით S_A_Q_A_R_T_V_E_L_O
+# შეეკითხეთ მომხმარებელს ქართველი ფეხბურთელების სახელები 12 ჯერ და დაამატეთ სიაში
+
+# S_A_Q_A_R_T_V_E_L_O = []
+# for i in range(12):
+#     name = input("sheiyvane qartveli fexburtelis saxeli: ")
+#     S_A_Q_A_R_T_V_E_L_O.append(name)
+# print(S_A_Q_A_R_T_V_E_L_O)
