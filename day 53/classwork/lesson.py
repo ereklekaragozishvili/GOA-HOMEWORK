786c04d23d42805bd8ea3197a7ee7fd9eadc36b0
ereklekaragozishvili <erekleyaragozishvili61@gmail.com>
2024-08-13 12:34:25 +0400

    commit

---
+++ day 53/classwork/lesson.py
@@ -0,0 +1,17 @@
+#  1) შექმენით ფუნქცია რომელიც ასახავთ თქვენი სახლიდან მაღაზიაში წასვლა
+# და ეს ფუნქცია უნდა გამოიძახოთ მრავლობით ჯერ 
+
+def saxlidan_wasvla():
+    print("saxlidan wavedi")
+saxlidan_wasvla()
+saxlidan_wasvla()
+saxlidan_wasvla()
+saxlidan_wasvla()
+
+    
+
+
+
+
+
+
---
+++ day 53/homework/lesson.py
@@ -0,0 +1,94 @@
+#   1) შექმენით კალკულატორის ფუნქცია  რომელიც მომხმარებელს შეეკითხება ორ რიცხვს და
+#  მოქმედების ტიპს(მიმატება, გამოკლება, გამრავლება, გაყოფა) 
+# შესაბამისად ფუნქცია დააბრუნებს
+# ანუ დაბეჭდავს შედეგს იმისდამიხედვით მომხმარებელს რა სურს და რა რიცხვები შემოიტანა, 
+# მაგალითად მომხმარებელმა თუ შემოიტანა 5 და 2 და ასევე მას სურს გამრავლება ფუნქციამ 
+# უნდა დააბრუნოს დაბეჭდოს შედეგად 2 * 5 ანუ 10
+#     2) შექმენით ფუნქცია რომელშიც იქნება რიცხვებით სავსე სია და ფუნქცია დააბრუნებს ამ რიცხვების ჯამს
+#     3) შექმენით ფუნქცია რომელშიც დააბრუნებს სიაში არსებული რიცხვების საშუალო არითმეტიკულკს
+#     4) შექმენით ფუნქცია რომელიც მომხმარებელს შეეკითხება რიცხვს და შემდეგ კი დააბრუნებს დაბეჭდავს მომხმარებლის შემოტანილი რიცხვი კენტია თუ ლუწი
+#     5) შექმენით დროში მოგზაურობის ფუნქცია რომელიც მომხმარებელს შეეკითხება მის ახლანდელ ასაკს და ამჟამინდელ წელს,
+#  ასევე რამდენი ხანით სურს დროშ 
+# მოგზაურობა შემდეგ კი ფუნქციამ უნდა
+# დააბრუნოს დაბეჭდოს დროში მოგზაურობის შემდეგ რომელი წელი იქნება და რამდენი წლის იქნება მომხმარებელი,
+# ასევე დაამატეთ რომ მომხმარებელმა მიიღოს გადაწყვეტილება წარსულში სურს დრში 
+# მოგზაურობა თუ მომავალშ
+
+def droshi_mogzauroba():
+   age = int(input("enter your age: "))
+   year = int(input("enter year: "))
+   time_travel = int(input("how long do you want to travel for?: "))
+   future = input("do you want to travel in the future: ")
+  if 
+   
+   
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+def kalkulatoris_funqcia():
+  number1 = int(input("enter number1: "))
+  number2 = int(input("enter number2: "))
+  user_swich = input("Enter operator: ")
+  if user_swich == '-':
+    print(number1 - number2)
+  if user_swich == '+':
+    print(number1 + number2)
+  if user_swich == '*':
+    print(number1 * number2)
+  if user_swich == '/':
+    print(number1 / number2)
+  
+# kalkulatoris_funqcia()
+
+  
+
+
+
+def ricxvebit_savse_sia():
+  numbers  = [1,2,3,4,]
+  counter = 0
+  for i in numbers:
+    counter += i
+  print(counter)
+
+
+# ricxvebit_savse_sia()
+
+
+def sahsvalo_aritmetikuli():
+    numbers = [1,2,3,4,5]
+    counter = 0 
+    for i in numbers:
+      counter += i
+    print(counter / len(numbers))
+
+# sahsvalo_aritmetikuli()
+
+
+
+def shekitxva_momxmarebels():
+   numbers1 = int(input("enter numbers: "))
+   if numbers1 % 2 == 0:
+     print('even')
+   else:
+     print('odd')
+# shekitxva_momxmarebels()     
+    
+
+
+
+
+
+
